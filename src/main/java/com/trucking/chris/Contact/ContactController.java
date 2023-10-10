af5b5671d950285fd5cae8c76efe11f2fc2cc82d
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-09 20:20:19 -0500

    MarquisTheCoder:  Mon Oct  9 20:20:19 CDT 2023

---
+++ src/main/java/com/trucking/chris/Contact/ContactController.java
@@ -0,0 +1,13 @@
+package com.trucking.chris.Contact;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+    @GetMapping("/contact")
+    public String Contact(){
+        return "contact";
+    }
+}
