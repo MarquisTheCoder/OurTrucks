4562949a56cd63f7875907442d2661439cb739cf
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-10-14 04:21:25 -0500

    MarquisTheCoder:  Sat Oct 14 04:21:25 CDT 2023

--- src/main/java/com/trucking/chris/ChrisApplication.java
+++ src/main/java/com/trucking/chris/ChrisApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChrisApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChrisApplication.class, args);
 	}
